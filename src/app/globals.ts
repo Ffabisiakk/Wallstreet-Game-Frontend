77a7e45bbf93a579c805a62e52e3d2fde00dcbd2
Ffabisiakk <ffabisiakk@gmail.com>
2020-02-04 23:36:02 +0100

    styling + html structure

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -1,1 +1,2 @@
-export const endpoint = 'https://wallstreet-app.herokuapp.com/';
+// export const endpoint = 'https://wallstreet-app.herokuapp.com/';
+export const endpoint = 'http://localhost:8080/';
